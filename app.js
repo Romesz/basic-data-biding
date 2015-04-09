c7464cd4b998e794b5fc362bde95c1c6ef55b0dd
Romano Firtermaiszter <rfirtermaiszter@performgroup.com>
2015-04-09 11:46:25 +0200

    ident app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ var cheerio = require('cheerio');
 var url = 'http://www.imdb.com/title/tt1853728/';
 	
 request(url, function (error, response, body) {
-	if(!error && response.statusCode == 200) {
-  var $ = cheerio.load(body);
-  var movieName = $('.itemprop').html();
-  var movieRate = $('.titlePageSprite').html();
-  var description = $('p[itemprop="description"]').html();
-  console.log('It’s ' + movieName + ' and rating: ' + movieRate);
-  console.log('The description is: ' + description);
-	}
-	else {
-		console.log('Error: ' + error);
-	}
+    if(!error && response.statusCode == 200) {
+        var $ = cheerio.load(body);
+        var movieName = $('.itemprop').html();
+        var movieRate = $('.titlePageSprite').html();
+        var description = $('p[itemprop="description"]').html();
+        console.log('It’s ' + movieName + ' and rating: ' + movieRate);
+        console.log('The description is: ' + description);
+    }
+    else {
+        console.log('Error: ' + error);
+    }
 });
