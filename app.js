48c86c8cbfef07711fee8f0fc33ad7b53ff311da
Romano Firtermaiszter <rfirtermaiszter@performgroup.com>
2015-04-09 11:40:40 +0200

    add files

---
+++ app.js
@@ -0,0 +1,17 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var url = 'http://www.imdb.com/title/tt1853728/';
+	
+request(url, function (error, response, body) {
+	if(!error && response.statusCode == 200) {
+  var $ = cheerio.load(body);
+  var movieName = $('.itemprop').html();
+  var movieRate = $('.titlePageSprite').html();
+  var description = $('p[itemprop="description"]').html();
+  console.log('It’s ' + movieName + ' and rating: ' + movieRate);
+  console.log('The description is: ' + description);
+	}
+	else {
+		console.log('Error: ' + error);
+	}
+});
